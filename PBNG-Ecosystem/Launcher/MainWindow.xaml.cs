d67211b4ca46f41cbe4c221f00ea099e767ad522
admin <admin@ngshp.com>
2026-09-02 19:13:37 +0700

    Refactor MainWindow to App and initialize services

--- PBNG-Ecosystem/Launcher/MainWindow.xaml.cs
+++ PBNG-Ecosystem/Launcher/MainWindow.xaml.cs
@@ -1,34 +1,29 @@
+using System;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace PBNG.Launcher
 {
-    public partial class MainWindow : Window
+    public partial class App : System.Windows.Application
     {
-        public MainWindow()
+        public static Services.DiscordService? Discord { get; private set; }
+        public static Services.UpdaterService? Updater { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
         {
-            this.Title = "PBNG Launcher";
-            this.Width = 900;
-            this.Height = 600;
-            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            base.OnStartup(e);
+            
+            Discord = new Services.DiscordService();
+            Discord.Init();
+            Updater = new Services.UpdaterService();
 
-            var grid = new Grid();
-            var text = new TextBlock
-            {
-                Text = "PBNG Launcher - Build SUCCESS! 💚",
-                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-                FontSize = 24
-            };
-            grid.Children.Add(text);
-            this.Content = grid;
-
-            this.Loaded += MainWindow_Loaded;
+            var main = new MainWindow();
+            main.Show();
         }
 
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        protected override void OnExit(ExitEventArgs e)
         {
-            App.Discord?.SetPresence("In PBNG Launcher", "Browsing Games");
+            Discord?.Dispose();
+            base.OnExit(e);
         }
     }
 }
