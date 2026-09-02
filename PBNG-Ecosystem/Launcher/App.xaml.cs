24bdccc603df20248e829bbc2cef89dd5ebfe084
admin <admin@ngshp.com>
2026-09-03 00:53:32 +0700

    Refactor App class by removing service initializations
    Removed Discord and Updater service initialization from App class.

--- PBNG-Ecosystem/Launcher/App.xaml.cs
+++ PBNG-Ecosystem/Launcher/App.xaml.cs
@@ -1,29 +1,8 @@
-using System;
 using System.Windows;
 
 namespace PBNG.Launcher
 {
-    public partial class App : System.Windows.Application
+    public partial class App : Application
     {
-        public static Services.DiscordService? Discord { get; private set; }
-        public static Services.UpdaterService? Updater { get; private set; }
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            
-            Discord = new Services.DiscordService();
-            Discord.Init();
-            Updater = new Services.UpdaterService();
-
-            var main = new MainWindow();
-            main.Show();
-        }
-
-        protected override void OnExit(ExitEventArgs e)
-        {
-            Discord?.Dispose();
-            base.OnExit(e);
-        }
     }
 }
