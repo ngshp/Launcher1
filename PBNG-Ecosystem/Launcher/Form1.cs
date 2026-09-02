397b663de273951cba0b0c8f9dd897b6ee19850e
admin <admin@ngshp.com>
2026-09-02 15:40:31 +0700

    Update Form1.cs for Windows Forms and Discord RPC
    Added necessary directives for Windows Forms and Discord RPC.

--- PBNG-Ecosystem/Launcher/Form1.cs
+++ PBNG-Ecosystem/Launcher/Form1.cs
@@ -1,3 +1,4 @@
+using System.Windows.Forms;
 <UseWPF>true</UseWPF>
 <UseWindowsForms>true</UseWindowsForms> <!-- INI KUNCINYA BIAR Form1.cs BISA -->
 <PackageReference Include="DiscordRPC" Version="1.0.0-b20190621" />
