cf89d832f81e2a48299223f307ffaac7cf2375f5
admin <admin@ngshp.com>
2026-09-02 15:19:20 +0700

    Remove Discord RPC initialization from Program.cs
    Removed Discord RPC initialization code from Main method.

--- PBNG-Ecosystem/Launcher/Program.cs
+++ PBNG-Ecosystem/Launcher/Program.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Windows;
-using PBNG.Launcher.Services;
 
 namespace PBNG.Launcher
 {
@@ -9,15 +8,6 @@ namespace PBNG.Launcher
         [STAThread]
         public static void Main()
         {
-            // Init Discord RPC
-            var discord = new DiscordService();
-            try
-            {
-                // GANTI dengan ID asli dari discord.com/developers/applications
-                discord.Init("1234567890123456789");
-            }
-            catch {}
-
             var app = new App();
             app.InitializeComponent();
             app.Run();
