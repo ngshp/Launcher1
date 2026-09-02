5522388038956cb5cbd85aa132db00c9db740b3f
admin <admin@ngshp.com>
2026-09-02 16:50:13 +0700

    Refactor App class and initialize services
    Refactor App class to use System.Windows.Application and initialize Discord and Update services on startup.

--- PBNG-Ecosystem/Launcher/App.xaml.cs
+++ PBNG-Ecosystem/Launcher/App.xaml.cs
@@ -1,8 +1,26 @@
 using System.Windows;
+using PBNG.Launcher.Services;
 
 namespace PBNG.Launcher
 {
-    public partial class App : Application
+    // FIX: Pakai System.Windows.Application biar gak bentrok sama WinForms
+    public partial class App : System.Windows.Application
     {
+        public static DiscordService? Discord { get; private set; }
+        public static UpdateService? Updater { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Discord = new DiscordService();
+            Discord.Init();
+            Updater = new UpdateService();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Discord?.Dispose();
+            base.OnExit(e);
+        }
     }
 }
