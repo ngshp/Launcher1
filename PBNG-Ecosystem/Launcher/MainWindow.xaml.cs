0fa120b6ef8e274932e8308603e0cb265f5e8529
admin <admin@ngshp.com>
2026-09-02 15:03:07 +0700

    Integrate Discord and update services in MainWindow
    Added Discord and update services to MainWindow.

--- PBNG-Ecosystem/Launcher/MainWindow.xaml.cs
+++ PBNG-Ecosystem/Launcher/MainWindow.xaml.cs
@@ -1,11 +1,38 @@
 using System.Windows;
+using PBNG.Launcher.Services;
 
-namespace Launcher;
-
-public partial class MainWindow : Window
+namespace PBNG.Launcher
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private DiscordService _discord = new();
+        private UpdateService _updater = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            Closing += (s, e) => _discord.Dispose();
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try { _discord.Init("1234567890123456789"); } catch {}
+
+            try
+            {
+                var (hasUpdate, ver, url) = await _updater.CheckAsync();
+                if (hasUpdate)
+                {
+                    var res = MessageBox.Show($"Update v{ver} tersedia! Update sekarang?", "PBNG Launcher", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                    if (res == MessageBoxResult.Yes)
+                    {
+                        _discord.SetPresence($"Updating to v{ver}", "Downloading...");
+                        await _updater.DownloadAndInstallAsync(url);
+                    }
+                }
+            }
+            catch {}
+        }
     }
 }
