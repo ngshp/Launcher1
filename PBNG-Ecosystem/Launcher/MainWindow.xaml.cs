40f81397399b8ddb870315e8d3c1367be9b0115c
admin <admin@ngshp.com>
2026-09-02 15:41:36 +0700

    Refactor MainWindow and update DiscordService usage
    Refactor MainWindow to use nullable DiscordService and update event handlers.

--- PBNG-Ecosystem/Launcher/MainWindow.xaml.cs
+++ PBNG-Ecosystem/Launcher/MainWindow.xaml.cs
@@ -5,34 +5,19 @@ namespace PBNG.Launcher
 {
     public partial class MainWindow : Window
     {
-        private DiscordService _discord = new();
-        private UpdateService _updater = new();
+        private DiscordService? _discord;
 
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += MainWindow_Loaded;
-            Closing += (s, e) => _discord.Dispose();
+            Loaded += OnLoaded;
+            Closed += (s, e) => _discord?.Dispose();
         }
 
-        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        private void OnLoaded(object sender, RoutedEventArgs e)
         {
-            try { _discord.Init("1234567890123456789"); } catch {}
-
-            try
-            {
-                var (hasUpdate, ver, url) = await _updater.CheckAsync();
-                if (hasUpdate)
-                {
-                    var res = MessageBox.Show($"Update v{ver} tersedia! Update sekarang?", "PBNG Launcher", MessageBoxButton.YesNo, MessageBoxImage.Information);
-                    if (res == MessageBoxResult.Yes)
-                    {
-                        _discord.SetPresence($"Updating to v{ver}", "Downloading...");
-                        await _updater.DownloadAndInstallAsync(url);
-                    }
-                }
-            }
-            catch {}
+            _discord = new DiscordService();
+            _discord.Init();
         }
     }
 }
