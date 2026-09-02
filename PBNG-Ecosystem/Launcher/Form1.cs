50f56a8353d251c67cf411098e07a4f08223da52
admin <admin@ngshp.com>
2026-09-02 15:36:42 +0700

    Refactor Form1.cs to remove services and add references
    Removed Discord and update services from Form1.cs and added WPF and WindowsForms references.

--- PBNG-Ecosystem/Launcher/Form1.cs
+++ PBNG-Ecosystem/Launcher/Form1.cs
@@ -1,21 +1,3 @@
-using PBNG.Launcher.Services;
-
-public partial class Form1 : Form
-{
-    private DiscordService discord = new();
-    private UpdateService updater = new();
-
-    private async void Form1_Load(object sender, EventArgs e)
-    {
-        try { discord.Init("1234567890123456789"); } catch {}
-        discord.SetPresence("In PBNG Launcher", "Browsing");
-
-        var (hasUpdate, ver, url) = await updater.CheckAsync();
-        if (hasUpdate && MessageBox.Show($"Update v{ver} tersedia! Update?", "PBNG", MessageBoxButtons.YesNo) == DialogResult.Yes)
-        {
-            await updater.DownloadAndInstallAsync(url);
-        }
-    }
-
-    private void Form1_FormClosing(object sender, FormClosingEventArgs e) => discord.Dispose();
-}
+<UseWPF>true</UseWPF>
+<UseWindowsForms>true</UseWindowsForms> <!-- INI KUNCINYA BIAR Form1.cs BISA -->
+<PackageReference Include="DiscordRPC" Version="1.0.0-b20190621" />
